d3a3fec66e2c28ad51d4d2002d657bc13b80c613
Sikho Qangule <sqangule@c4r10s10.wethinkcode.co.za>
2020-06-21 16:44:17 +0200

    ex06 & ex07

---
+++ ex07/ft_find_next_prime.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqangule <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 15:02:53 by sqangule          #+#    #+#             */
+/*   Updated: 2020/06/21 15:16:56 by sqangule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		is_prime(int nb)
+{
+	int		d;
+
+	if (nb < 2)
+		return (0);
+	d = 1;
+	while (++d < nb)
+		if (!(nb % d))
+			return (0);
+	return (1);
+}
+
+int		ft_find_next_prime(int nb)
+{
+	if (is_prime(nb))
+		return (nb);
+	return (ft_find_next_prime(nb + 1));
+}
