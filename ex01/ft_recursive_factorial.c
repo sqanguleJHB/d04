d55de612e7e66c3dff9eb8176b849855a13429a1
Sikho Qangule <sqangule@c4r10s10.wethinkcode.co.za>
2020-06-25 12:54:14 +0200

    .

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: sqangule <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 17:22:34 by sqangule          #+#    #+#             */
-/*   Updated: 2020/06/19 17:26:43 by sqangule         ###   ########.fr       */
+/*   Updated: 2020/06/25 12:53:11 by sqangule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int		ft_recursive_factorial(int nb)
 {
 	if (nb < 0 || nb > 12)
 		return (0);
-	else if (nb == 1)
+	else if (nb == 0)
 		return (1);
 	return (nb * ft_recursive_factorial(nb - 1));
 }
